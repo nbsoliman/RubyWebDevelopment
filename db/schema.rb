8d9053e348cf4ab54429f6763078d50f098f6dbe
nbsoliman <nbsoliman18@gmail.com>
2021-09-27 13:26:34 -0500

    minor changes

--- db/schema.rb
+++ db/schema.rb
@@ -17,6 +17,7 @@ ActiveRecord::Schema.define(version: 2021_09_17_034806) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
+    
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
