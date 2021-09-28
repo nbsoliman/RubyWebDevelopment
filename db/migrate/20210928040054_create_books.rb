b53d1d281dff1cd6bbbcde0e314352e36616617a
root <root@localhost>
2021-09-28 04:01:44 +0000

    Added Author, Price, and Publish Date

--- db/migrate/20210928040054_create_books.rb
+++ db/migrate/20210928040054_create_books.rb
@@ -3,6 +3,8 @@ class CreateBooks < ActiveRecord::Migration[6.1]
     create_table :books do |t|
       t.string :title
       t.string :author
+      t.float :price
+      t.date :publishDate
 
       t.timestamps
     end
