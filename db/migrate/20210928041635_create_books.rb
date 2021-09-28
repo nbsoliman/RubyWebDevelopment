340844847a7d130bef732e3c32600dcc5002311b
root <root@localhost>
2021-09-28 04:17:55 +0000

    Added other elements

--- db/migrate/20210928041635_create_books.rb
+++ db/migrate/20210928041635_create_books.rb
@@ -4,7 +4,7 @@ class CreateBooks < ActiveRecord::Migration[6.1]
       t.string :title
       t.string :author
       t.float :price
-      t.date :publishDate
+      t.datetime :publishdate
 
       t.timestamps
     end
